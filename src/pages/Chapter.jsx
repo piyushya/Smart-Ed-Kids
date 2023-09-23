44cca291df04445938b4cf2ed8956f95c6b45c16
Piyush Yadav <piyushya012@gmail.com>
2023-09-23 13:00:35 +0530

    removed window onload

--- src/pages/Chapter.jsx
+++ src/pages/Chapter.jsx
@@ -16,12 +16,12 @@ export default function Chapter(){
     const navigate = useNavigate();
     
     useEffect(() => {
-        window.onload = () => {
+        
             // Simulate a minimum display time of 1 second
             setTimeout(() => {
               setPageLoading(false);
             }, 1000); // Minimum display time of 1 second
-          };
+          
     }, [])
 
     return (
