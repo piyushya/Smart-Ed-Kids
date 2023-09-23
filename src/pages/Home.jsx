196964b0266d101082b6b3111bd015cce651693a
Piyush Yadav <piyushya012@gmail.com>
2023-09-23 12:59:42 +0530

    removed window onload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,12 +31,12 @@ export default function Home(){
     }, [user, loading]);
 
     useEffect(() => {
-        window.onload = () => {
+        
             // Simulate a minimum display time of 1 second
             setTimeout(() => {
               setPageLoading(false);
             }, 1000); // Minimum display time of 1 second
-          };
+          
     }, [])
 
     const [profile, setProfile] = useState({
